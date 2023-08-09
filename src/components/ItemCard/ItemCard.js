dee4a6ca891933bf8ffa4a81eb01baaf39151ca6
MarsiaRochelin <marsrochelin@pursuit.org>
2023-08-08 20:39:44 -0400

    func created to render topping content conditionally

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -3,11 +3,18 @@ import "./ItemCard.css";
 const ItemCard = ({ item }) => {
   const { name, shortDescription, image, price, toppings } = item;
 
-  //create a function for toppings to conditionally render toppings based on length of array
-  //if toppings length is equal to zero show no text content for toppings
-  //else return text content for toppings followed bu a colon seperated by a space listing out each toppings seperated by a comma and a sapce for each item in the toppings array aceept the last one
-
-  //add a space between the toppings text content and the topping
+  const toTopOrNot = (toppings) => {
+    if (toppings.length >= 1) {
+      return (
+        <li>
+          Toppings: {""}
+          {toppings.join(", ")}
+        </li>
+      );
+    } else {
+      return "";
+    }
+  };
 
   return (
     <div className="ItemCard">
@@ -16,10 +23,7 @@ const ItemCard = ({ item }) => {
       <br />
       <img src={image} alt={name} />
       <li className="line-break">{price}</li>
-      <li>
-        Toppings:
-        {toppings.join(", ")}
-      </li>
+      {toTopOrNot(toppings)}
     </div>
   );
 };
